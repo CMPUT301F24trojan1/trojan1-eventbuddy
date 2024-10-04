5de6835000c80343e6df1d250b93571824df7f5b
Jared Gourley <jgourley@ualberta.ca>
2024-10-04 17:50:21 -0600

    Created Android Studio project with firebase and safeArgs configured

---
+++ code/build.gradle.kts
@@ -0,0 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.7.6"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
