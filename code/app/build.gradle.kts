746734fbf39ef21adf740c3bff0956f925063386
Jared Gourley <jgourley@ualberta.ca>
2024-10-19 17:52:52 -0600

    Edited app level gradle

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage")
 
     implementation(libs.appcompat)
     implementation(libs.material)
