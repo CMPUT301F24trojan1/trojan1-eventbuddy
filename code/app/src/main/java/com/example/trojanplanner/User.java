a7741fe50b26cce43bb4f7120ec6b281d6cd73e9
Madelaine Dalangin <madelainedalangin@gmail.com>
2024-10-19 19:06:41 -0600

    Added methods isAdmin and isOrganizer, modified deviceId, added comments

--- code/app/src/main/java/com/example/trojanplanner/User.java
+++ code/app/src/main/java/com/example/trojanplanner/User.java
@@ -1,58 +1,167 @@
 package com.example.trojanplanner;
+import android.provider.Settings;
 
+/**
+ * user class contains and stores information tied to a user.
+ * @author Madelaine Dalangin
+ */
 public class User {
-    private String name;
+    private String lastName;
+    private String firstName;
     private String email;
     private String phoneNumber;
-    private String deviceID;
-    private String role;
+    private String deviceId;
+    private String role; //Entrant, Organizer, Admin
+    private boolean isOrganizer;
+    private boolean isAdmin;
 
-    //Constructor
-    public User(String name, String email, String phoneNumber, String deviceID, String role){
-        this.name = name;
+    /**
+     * Constructor Method for User
+     * @author Madelaine Dalangin
+     * @param lastName String
+     * @param firstName String
+     * @param email String
+     * @param phoneNumber String
+     * @param deviceId String
+     * @param role String
+     * @param isOrganizer boolean
+     * @param isAdmin boolean
+     */
+    public User(String lastName, String firstName, String email, String phoneNumber, String deviceId, String role, boolean isOrganizer, boolean isAdmin){
+        this.lastName = lastName;
+        this.firstName = firstName;
         this.email = email;
         this.phoneNumber = phoneNumber;
-        this.deviceID = deviceID;
+        this.deviceId = Settings.Secure.ANDROID_ID;
         this.role = role;
+        this.isOrganizer = isOrganizer;
+        this.isAdmin = isAdmin;
     }
 
-    public String getName() {
-        return name;
+    /**
+     * Getter method for user's last name
+     * @author Madelaine Dalangin
+     * @return lastName string
+     */
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    /**
+     * Setter method for user's last name
+     * @author Madelaine Dalangin
+     * @param lastName String
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
+    /**
+     * Getter method for user's first name
+     * @author Madelaine dalangin
+     * @return firstName String
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Setter method for user's first name
+     * @author Madelaine Dalangin
+     * @param firstName String
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * String method for User name
+     * @author Madelaine Dalangin
+     * @return email string
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Setter method for user's email address
+     * @author Madelaine Dalangin
+     * @param email String
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Getter method for user's phone number
+     * @author Madelaine Dalangin
+     * @return phoneNumber string
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Setter method for user's phone number
+     * @author Madelaine Dalangin
+     * @param phoneNumber String
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getDeviceID() {
-        return deviceID;
+    /**
+     * Getter method for User's device ID
+     * @author Madelaine Dalangin
+     * @return deviceId String
+     */
+    public String getDeviceId() {
+        return deviceId;
     }
 
-    public void setDeviceID(String deviceID) {
-        this.deviceID = deviceID;
+    /**
+     * Setter method for user's device ID
+     * @author Madelaine Dalangin
+     * @param deviceId String
+     */
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
     }
 
+    /**
+     * Getter method for user's role
+     * @author Madelaine Dalangin
+     * @return role String
+     */
     public String getRole() {
         return role;
     }
 
+    /**
+     * Setter method for User's role
+     * @author Madelaine Dalangin
+     * @param role string
+     */
     public void setRole(String role) {
         this.role = role;
     }
+
+    /**
+     * Boolean method if user is also an organizer
+     * @author Madelaine Dalangin
+     * @return isOrganizer boolean
+     */
+    public boolean isOrganizer(){
+        return isOrganizer;
+    }
+
+    /**
+     * Boolean method if user is also an admin
+     * @author Madelaine Dalangin
+     * @return isAdmin boolean
+     */
+    public boolean isAdmin() {
+        return isAdmin;
+    }
 }
+
