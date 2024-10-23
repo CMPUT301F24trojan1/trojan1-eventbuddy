c5630ad40464707b5e37e2808542a875b24a44d0
Madelaine Dalangin <madelainedalangin@gmail.com>
2024-10-22 22:00:49 -0600

    changed array methods to private after modifications and added comments

--- code/app/src/main/java/com/example/trojanplanner/Participant.java
+++ code/app/src/main/java/com/example/trojanplanner/Participant.java
@@ -7,7 +7,9 @@ public class Participant extends User{
 
 
     private boolean checkInStatus;
-    private ArrayList<Event> currentEventsRegistered;
+    private ArrayList<Event> currentRegisteredEvents;
+    private ArrayList<Event> currentJoinedEvents;
+    private ArrayList<Event> currentPendingEvents;
 
     /**
      * Constructor Method for User
@@ -25,25 +27,80 @@ public class Participant extends User{
     public Participant(String lastName, String firstName, String email, String phoneNumber, String deviceId, String role, boolean isOrganizer, boolean isAdmin, Event event, boolean checkInStatus) {
         super(lastName, firstName, email, phoneNumber, deviceId, role, isOrganizer, isAdmin);
         this.checkInStatus = checkInStatus;
-        this.currentEventsRegistered = new ArrayList<Event>();
+        this.currentRegisteredEvents = new ArrayList<Event>();
     }
 
-    public void addEvent(Event event){
-        if(!currentEventsRegistered.contains(event)) {
-            currentEventsRegistered.add(event);
+    /**
+     * Method for adding event to Registered events array
+     * @author Madelaine Dalangin
+     * @param event
+     */
+    private void addRegisteredEvent(Event event){
+        if(!currentRegisteredEvents.contains(event)) {
+            currentRegisteredEvents.add(event);
         }
         throw new IllegalArgumentException("Participant is already registered to event");
     }
 
-    public void removeEvent(Event event){
-        if(currentEventsRegistered.contains(event)){
-            currentEventsRegistered.remove(event);
+    /**
+     * Method for removing an event a participant registered in
+     * @author Madelaine Dalangin
+     * @param event
+     */
+    private void removeRegisteredEvent(Event event){
+        if(currentRegisteredEvents.contains(event)){
+            currentRegisteredEvents.remove(event);
         }
         throw new IllegalArgumentException("Event doesn't exist in the list.");
     }
 
-    public boolean isCheckInStatus() {
-        return checkInStatus;
+    /**
+     * Adding event to array of Participant's joined events
+     * @author Madelaine Dalangin
+     * @param event
+     */
+    private void addJoinedEvent(Event event){
+        if(!currentJoinedEvents.contains(event)){
+            currentJoinedEvents.add(event);
+        }
+        throw new IllegalArgumentException("Participant already confirmed to join event.");
     }
+
+    /**
+     * Method for removing an event Participant has joined in (picked on raffle and participant joined)
+     * @author Madelaine Dalangin
+     * @param event
+     */
+    private void removeJoinedEvent(Event event){
+        if(currentJoinedEvents.contains(event)){
+            currentJoinedEvents.remove(event);
+        }
+        throw new IllegalArgumentException("Participant already opted out of event.");
+    }
+
+    /**
+     * Method for array of events participant has yet to confirm rsvp or deny
+     * @author Madelaine Dalangin
+     * @param event
+     */
+    private void addPendingEvent(Event event){
+        if(!currentPendingEvents.contains(event)){
+            currentPendingEvents.add(event);
+        }
+        throw new IllegalArgumentException("Event in Participant's Pending list.");
+    }
+
+    /**
+     * Method for removing events participant was picked from but they denied to join
+     * @author Madelaine Dalangin
+     * @param event
+     */
+    private void removePendingEvent(Event event){
+        if(currentPendingEvents.contains(event)){
+            currentPendingEvents.remove(event);
+        }
+        throw new IllegalArgumentException("Event does not exist in pending list.");
+    }
+
 
 }
