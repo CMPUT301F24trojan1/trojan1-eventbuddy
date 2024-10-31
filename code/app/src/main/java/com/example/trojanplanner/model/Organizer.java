6d1fc09023cf664c366fcbcd6bf0856edaf75fd4
Madelaine Dalangin <madelainedalangin@gmail.com>
2024-10-31 17:20:21 -0600

    Initial Commit Organizer

--- code/app/src/main/java/com/example/trojanplanner/model/Organizer.java
+++ code/app/src/main/java/com/example/trojanplanner/model/Organizer.java
@@ -1,2 +1,13 @@
-package com.example.trojanplanner.model;public class Organizer {
+package com.example.trojanplanner.model;
+
+public class Organizer extends User{
+    private Entrant nameOfOrganizer;
+    private Event nameOfEvent; //should this be Event class and not string?
+
+    public Organizer(String lastName, String firstName, String email, String phoneNumber, String deviceId, String role, boolean isOrganizer, boolean isAdmin, Entrant nameOfOrganizer, Event nameOfEvent) {
+        super(lastName, firstName, email, phoneNumber, deviceId, role, isOrganizer, isAdmin);
+        this.nameOfOrganizer = nameOfOrganizer;
+        this.nameOfEvent = nameOfEvent;
+    }
+
 }
