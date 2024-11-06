c6b02cbff18af60af7384ea7a998e883289efad5
Madelaine Dalangin <madelainedalangin@gmail.com>
2024-11-05 17:15:34 -0700

    added Facilities attribute and methods to Organizer

--- code/app/src/main/java/com/example/trojanplanner/model/Organizer.java
+++ code/app/src/main/java/com/example/trojanplanner/model/Organizer.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 
 public class Organizer extends User {
     private ArrayList<Event> createdEvents;
+    private ArrayList<Facility> createdFacility;
 
-    public Organizer(String lastName, String firstName, String email, String phoneNumber, String deviceId, String role, boolean isOrganizer, boolean isAdmin, ArrayList<Event> createdEvents) {
+    public Organizer(String lastName, String firstName, String email, String phoneNumber, String deviceId, String role, boolean isOrganizer, boolean isAdmin, ArrayList<Event> createdEvents, ArrayList<Facility> createdFacility) {
         super(lastName, firstName, email, phoneNumber, deviceId, role, isOrganizer, isAdmin);
         this.createdEvents = createdEvents;
+        this.createdFacility = createdFacility;
     }
 
     public ArrayList<Event> getCreatedEvents() {
@@ -38,6 +40,26 @@ public class Organizer extends User {
         }
         else {
             throw new IllegalArgumentException("Invalid index on event delete");
+        }
+    }
+
+    public ArrayList<Facility> getCreatedFacility() {
+        return createdFacility;
+    }
+
+    public void createFacility(Facility facility) {
+        if (!createdFacility.contains(facility)){
+            createdFacility.add(facility);
+        } else {
+            throw new IllegalArgumentException("Facility already created.");
+        }
+    }
+
+    public void removeFacility(int index){
+        if(index < createdFacility.size() && index >= 0){
+            createdFacility.remove(index)
+        } else {
+            throw new IllegalArgumentException("Facility does not exist in the list.");
         }
     }
 
