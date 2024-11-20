01ce920d2f29847265b54be2d46ad9ab46c0dfdd
Fairuz Zarif <fairuzzarif7@gmail.com>
2024-11-20 06:30:06 -0700

    Fished profile test and fixed fragment navigation test. -These two main test should be running now -Event creation test needs to be added.

--- code/app/src/androidTest/java/com/example/trojanplanner/ProfileFragmentTest.java
+++ code/app/src/androidTest/java/com/example/trojanplanner/ProfileFragmentTest.java
@@ -2,17 +2,16 @@ package com.example.trojanplanner;
 
 import android.content.Intent;
 
-import androidx.fragment.app.testing.FragmentScenario;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
 
-import com.example.trojanplanner.ProfileUtils.ProfileFragment;
 import com.example.trojanplanner.view.MainActivity;
+import com.example.trojanplanner.view.ProfileActivity;
 
 import org.junit.After;
 import org.junit.Before;
@@ -21,122 +20,120 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.is;
 
 @RunWith(AndroidJUnit4.class)
-@LargeTest
 public class ProfileFragmentTest {
 
+    private ActivityScenario<MainActivity> activityScenario;
     @Rule
-    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
 
     @Before
     public void setUp() {
-        // Initialize the activity and fragment before running the tests
-        Intents.init();
-    }
-
-    @Test
-    public void testProfileFragmentUI() {
-        // Using Espresso to navigate to ProfileFragment
-        Espresso.onView(withId(R.id.nav_view)) // Bottom navigation view
-                .perform(ViewActions.click()); // Click the profile icon
-
-        // Check if ProfileFragment is displayed in the container
-        Espresso.onView(withId(R.id.profile_fragment_container))
-                .check(matches(ViewMatchers.isDisplayed()));
-    }
-
-    @Test
-    public void testProfileFragmentFormInteractions() {
-        // Navigate to ProfileFragment
-        Espresso.onView(withId(R.id.nav_view))
-                .perform(ViewActions.click());
-
-        // Interact with form inputs
-        Espresso.onView(withId(R.id.firstname_input))
-                .perform(clearText(), ViewActions.typeText("Test User"), ViewActions.closeSoftKeyboard());
-
-        Espresso.onView(withId(R.id.lastname_input))
-                .perform(clearText(), ViewActions.typeText("Test User1"), ViewActions.closeSoftKeyboard());
-
-        Espresso.onView(withId(R.id.email_input))
-                .perform(clearText(), ViewActions.typeText("test12@gmail.com"), ViewActions.closeSoftKeyboard());
-
-        Espresso.onView(withId(R.id.phone_input))
-                .perform(clearText(), ViewActions.typeText("123 123 1234"), ViewActions.closeSoftKeyboard());
-
-        // Verify fields contain the entered text
-        Espresso.onView(withId(R.id.firstname_input))
-                .check(matches(withText("Test User")));
-        Espresso.onView(withId(R.id.lastname_input))
-                .check(matches(withText("Test User1")));
-        Espresso.onView(withId(R.id.email_input))
-                .check(matches(withText("test12@gmail.com")));
-        Espresso.onView(withId(R.id.phone_input))
-                .check(matches(withText("123 123 1234")));
-    }
-
-    @Test
-    public void testSaveButton() {
-        // Navigate to ProfileFragment
-        Espresso.onView(withId(R.id.nav_view))
-                .perform(ViewActions.click());
-
-        // Interact with form inputs
-        Espresso.onView(withId(R.id.firstname_input))
-                .perform(clearText(), ViewActions.typeText("Test User"));
-
-        Espresso.onView(withId(R.id.save_button))
-                .perform(ViewActions.click());
-
-        // Verify that the "Document upload successful!" toast is shown
-        Espresso.onView(withText("Document upload successful!"))
-                .inRoot(withDecorView(not(is(activityRule.getActivity().getWindow().getDecorView())))) // Ensure this is a toast
-                .check(matches(ViewMatchers.isDisplayed())); // Ensure it's displayed
-    }
-
-    @Test
-    public void testCancelButton() {
-        // Navigate to ProfileFragment
-        Espresso.onView(withId(R.id.nav_view))
-                .perform(ViewActions.click());
-
-        // Enter some text into the fields
-        Espresso.onView(withId(R.id.firstname_input))
-                .perform(clearText(), ViewActions.typeText("Test User"), ViewActions.closeSoftKeyboard());
-
-        Espresso.onView(withId(R.id.lastname_input))
-                .perform(clearText(), ViewActions.typeText("Test User1"), ViewActions.closeSoftKeyboard());
-
-        Espresso.onView(withId(R.id.email_input))
-                .perform(clearText(), ViewActions.typeText("test12@gmail.com"), ViewActions.closeSoftKeyboard());
-
-        Espresso.onView(withId(R.id.phone_input))
-                .perform(clearText(), ViewActions.typeText("123 123 1234"), ViewActions.closeSoftKeyboard());
-
-        // Now click the Cancel button
-        Espresso.onView(withId(R.id.cancel_button))
-                .perform(ViewActions.click());
-
-        // Verify that the fields revert to their default values
-        Espresso.onView(withId(R.id.firstname_input))
-                .check(matches(withText("boe"))); // Default first name
-        Espresso.onView(withId(R.id.lastname_input))
-                .check(matches(withText("jdien"))); // Default last name
-        Espresso.onView(withId(R.id.email_input))
-                .check(matches(withText("boejiden@gmail.com"))); // Default email
-        Espresso.onView(withId(R.id.phone_input))
-                .check(matches(withText(""))); // Default phone number is empty
+        // Launch MainActivity before each test
+        activityScenario = ActivityScenario.launch(MainActivity.class);
+        Intents.init(); // Initialize Espresso Intents
     }
 
     @After
     public void tearDown() {
+        // Release Espresso Intents after each test
         Intents.release();
+        if (activityScenario != null) {
+            activityScenario.close();
+        }
+    }
+
+    @Test
+    public void testProfileFragmentNavigation() {
+        // Simulate a click on the Profile navigation button
+        Espresso.onView(withId(R.id.profileActivity)).perform(ViewActions.click());
+
+        // Verify that ProfileActivity is launched
+        Intents.intended(hasComponent(ProfileActivity.class.getName()));
+
+        //verify that ProfileActivity is in view
+        Espresso.onView(withId(R.id.profile_fragment_parent)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testProfileFragmentInput() {
+        // Navigate to the Profile Fragment or Activity
+        Espresso.onView(withId(R.id.profileActivity)).perform(ViewActions.click());
+
+        // Enter and verify text in the first name input field
+        Espresso.onView(withId(R.id.firstname_input)).perform(ViewActions.click(), clearText(), typeText("test user first name"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(withId(R.id.firstname_input)).check(matches(withText("test user first name")));
+
+        // Enter and verify text in the last name input field
+        Espresso.onView(withId(R.id.lastname_input)).perform(ViewActions.click(), clearText(), typeText("test user last name"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(withId(R.id.lastname_input)).check(matches(withText("test user last name")));
+
+        // Enter and verify text in the email input field
+        Espresso.onView(withId(R.id.email_input)).perform(ViewActions.click(), clearText(), typeText("testuser@gmail.com"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(withId(R.id.email_input)).check(matches(withText("testuser@gmail.com")));
+
+        // Enter and verify text in the phone input field
+        Espresso.onView(withId(R.id.phone_input)).perform(ViewActions.click(), clearText(), typeText("123 123 1234"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(withId(R.id.phone_input)).check(matches(withText("123 123 1234")));
+    }
+
+
+    @Test
+    public void testProfileSaveButton() {
+        // click on profile from nav menu
+        Espresso.onView(withId(R.id.profileActivity)).perform(ViewActions.click());
+
+        //check save button on first name text field
+        Espresso.onView(withId(R.id.firstname_input)).perform(ViewActions.click(), clearText(), typeText("test user first name"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(withId(R.id.save_button)).perform(ViewActions.click());
+        Espresso.onView(withId(R.id.firstname_input)).check(matches(withText("test user first name")));
+
+        //check save button on last name text field
+        Espresso.onView(withId(R.id.lastname_input)).perform(ViewActions.click(), clearText(), typeText("test user last name"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(withId(R.id.save_button)).perform(ViewActions.click());
+        Espresso.onView(withId(R.id.lastname_input)).check(matches(withText("test user last name")));
+
+        //check save button on email text field
+        Espresso.onView(withId(R.id.email_input)).perform(ViewActions.click(), clearText(), typeText("testUser@gmail.com"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(withId(R.id.save_button)).perform(ViewActions.click());
+        Espresso.onView(withId(R.id.email_input)).check(matches(withText("testUser@gmail.com")));
+
+        //check save button on phone text field
+        Espresso.onView(withId(R.id.phone_input)).perform(ViewActions.click(), clearText(), typeText("123 123 1234"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(withId(R.id.save_button)).perform(ViewActions.click());
+        Espresso.onView(withId(R.id.phone_input)).check(matches(withText("123 123 1234")));
+    }
+
+    @Test
+    public void testProfileCancelButton() {
+        // click on profile from nav menu
+        Espresso.onView(withId(R.id.profileActivity)).perform(ViewActions.click());
+
+        //check cancel button on first name text field
+        Espresso.onView(withId(R.id.firstname_input)).perform(ViewActions.click(), clearText(), typeText("test_user_first_name_2 (cancel test)"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(withId(R.id.cancel_button)).perform(ViewActions.click());
+        Espresso.onView(withId(R.id.firstname_input)).check(matches(withText("test user first name")));
+
+        //check cancel button on last name text field
+        Espresso.onView(withId(R.id.lastname_input)).perform(ViewActions.click(), clearText(), typeText("test_user_last_name_2 (cancel test)"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(withId(R.id.cancel_button)).perform(ViewActions.click());
+        Espresso.onView(withId(R.id.lastname_input)).check(matches(withText("test user last name")));
+
+        //check cancel button on email text field
+        Espresso.onView(withId(R.id.email_input)).perform(ViewActions.click(), clearText(), typeText("testUser2@gmail.com (cancel test)"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(withId(R.id.cancel_button)).perform(ViewActions.click());
+        Espresso.onView(withId(R.id.email_input)).check(matches(withText("testUser@gmail.com")));
+
+        //check cancel button on phone text field
+        Espresso.onView(withId(R.id.phone_input)).perform(ViewActions.click(), clearText(), typeText("321 321 4321"), ViewActions.closeSoftKeyboard());
+        Espresso.onView(withId(R.id.cancel_button)).perform(ViewActions.click());
+        Espresso.onView(withId(R.id.phone_input)).check(matches(withText("123 123 1234")));
     }
 }
