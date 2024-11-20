2f4398959876035a49490e664178e387cbe895eb
Fairuz Zarif <fairuzzarif7@gmail.com>
2024-11-20 06:30:06 -0700

    Fished profile test and fixed fragment navigation test. -These two main test should be running now -Event creation test needs to be added.

--- code/app/src/androidTest/java/com/example/trojanplanner/FragmentNavigationTest.java
+++ code/app/src/androidTest/java/com/example/trojanplanner/FragmentNavigationTest.java
@@ -2,11 +2,13 @@ package com.example.trojanplanner;
 
 import android.content.Intent;
 
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
@@ -20,79 +22,102 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
-@LargeTest
 public class FragmentNavigationTest {
 
+    private ActivityScenario<MainActivity> activityScenario;
     @Rule
-    public ActivityTestRule<MainActivity> mainActivityRule =
-            new ActivityTestRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
 
     @Before
     public void setUp() {
-        Intents.init();
-    }
-
-    @Test
-    public void testNavigationToEventsFragment() {
-        // Click on the events tab in the BottomNavigationView
-        //Espresso.onView(withId(R.id.eventsFragment)).perform(click());
-
-        // Check if the EventsFragment is displayed
-        Espresso.onView(withText("Event 1")).check(matches(ViewMatchers.isDisplayed()));
-    }
-
-    @Test
-    public void testNavigationToQRActivity() {
-        // Click on the QR tab in the BottomNavigationView
-        Espresso.onView(withId(R.id.qrActivity)).perform(click());
-
-        // Check if the QRActivity starts (check a specific view or feature of QRActivity)
-        //Espresso.onView(withId(R.id.barcode_scanner)).check(matches(ViewMatchers.isDisplayed()));
-    }
-
-    @Test
-    public void testNavigationToProfileActivity() {
-        // Click on the profile tab in the BottomNavigationView
-        Espresso.onView(withId(R.id.profileActivity)).perform(click());
-
-        // Check if the ProfileActivity starts (check a specific view or feature of ProfileActivity)
-        Espresso.onView(withId(R.id.profile_image)).check(matches(ViewMatchers.isDisplayed()));
-    }
-
-    @Test
-    public void testNavigationFromProfileActivity() {
-        // Start ProfileActivity
-        Intent profileIntent = new Intent(InstrumentationRegistry.getInstrumentation().getTargetContext(), ProfileActivity.class);
-        mainActivityRule.getActivity().startActivity(profileIntent);
-
-        // Click on the home navigation item in ProfileActivity's BottomNavigationView
-        Espresso.onView(withId(R.id.navigation_home)).perform(click());
-
-        // Check if it navigates back to MainActivity by checking if NavHostFragment is displayed
-        Espresso.onView(withId(R.id.nav_host_fragment_activity_main)).check(matches(ViewMatchers.isDisplayed()));
-    }
-
-    @Test
-    public void testNavigationFromQRActivity() {
-        // Start QRActivity
-        Intent qrIntent = new Intent(InstrumentationRegistry.getInstrumentation().getTargetContext(), QRActivity.class);
-        mainActivityRule.getActivity().startActivity(qrIntent);
-
-        // Click on the profile navigation item in QRActivity's BottomNavigationView
-        Espresso.onView(withId(R.id.profileActivity)).perform(click());
-
-        // Check if it navigates to ProfileActivity
-        Espresso.onView(withId(R.id.profile_image)).check(matches(ViewMatchers.isDisplayed()));
+        // Launch MainActivity before each test
+        activityScenario = ActivityScenario.launch(MainActivity.class);
+        Intents.init(); // Initialize Espresso Intents
     }
 
     @After
     public void tearDown() {
-        Intents.release(); // Clean up after the test
+        // Release Espresso Intents after each test
+        Intents.release();
+        if (activityScenario != null) {
+            activityScenario.close();
+        }
     }
+
+    @Test
+    public void testNavigationFromEventsFragment() {
+
+        //Click on the events tab in the BottomNavigationView
+        Espresso.onView(withId(R.id.navigation_home)).perform(ViewActions.click());
+        //Check if the EventsFragment is displayed
+        Espresso.onView(withId(R.id.empty_events_fragment_parent)).check(matches(ViewMatchers.isDisplayed()));
+
+        //Shift to qr fragment
+        Espresso.onView(withId(R.id.qrActivity)).perform(ViewActions.click());
+        //Check if the qr fragment is displayed
+        Espresso.onView(withId(R.id.activity_qr_parent)).check(matches(ViewMatchers.isDisplayed()));
+        //Espresso.onView(withId(R.id.profileActivity)).perform(ViewActions.click());
+
+        //Shift to profile fragment
+        Espresso.onView(withId(R.id.profileActivity)).perform(ViewActions.click());
+        //Check if the profile fragment is displayed
+        Espresso.onView(withId(R.id.profile_fragment_parent)).check(matches(ViewMatchers.isDisplayed()));
+
+    }
+
+    @Test
+    public void testNavigationFromQRFragment() {
+
+        Espresso.onView(withId(R.id.qrActivity)).perform(ViewActions.click());
+        //Check if the qr fragment is displayed
+        Espresso.onView(withId(R.id.activity_qr_parent)).check(matches(ViewMatchers.isDisplayed()));
+        //Espresso.onView(withId(R.id.profileActivity)).perform(ViewActions.click());
+
+        //Shift to profile fragment
+        Espresso.onView(withId(R.id.profileActivity)).perform(ViewActions.click());
+        //Check if the profile fragment is displayed
+        Espresso.onView(withId(R.id.profile_fragment_parent)).check(matches(ViewMatchers.isDisplayed()));
+
+        //Click on the events tab in the BottomNavigationView
+        Espresso.onView(withId(R.id.navigation_home)).perform(ViewActions.click());
+        //Check if the EventsFragment is displayed
+        Espresso.onView(withId(R.id.empty_events_fragment_parent)).check(matches(ViewMatchers.isDisplayed()));
+
+
+
+    }
+
+    @Test
+    public void testNavigationFromProfileFragment() {
+
+        Espresso.onView(withId(R.id.profileActivity)).perform(ViewActions.click());
+        //Check if the profile fragment is displayed
+        Espresso.onView(withId(R.id.profile_fragment_parent)).check(matches(ViewMatchers.isDisplayed()));
+
+        //Shift to qr fragment
+        Espresso.onView(withId(R.id.qrActivity)).perform(ViewActions.click());
+        //Check if the qr fragment is displayed
+        Espresso.onView(withId(R.id.activity_qr_parent)).check(matches(ViewMatchers.isDisplayed()));
+        //Espresso.onView(withId(R.id.profileActivity)).perform(ViewActions.click());
+
+        //Click on the events tab in the BottomNavigationView
+        Espresso.onView(withId(R.id.navigation_home)).perform(ViewActions.click());
+        //Check if the EventsFragment is displayed
+        Espresso.onView(withId(R.id.empty_events_fragment_parent)).check(matches(ViewMatchers.isDisplayed()));
+
+    }
+
+
+
+
 }
