583aeb9b0c1bba117bef223ce609a07ef5ad1587
Madelaine Dalangin <madelainedalangin@gmail.com>
2024-11-26 06:33:05 -0700

    Modified R.id name of constraint layout

--- code/app/src/main/java/com/example/trojanplanner/view/admin/AdminUsersActivity.java
+++ code/app/src/main/java/com/example/trojanplanner/view/admin/AdminUsersActivity.java
@@ -17,7 +17,7 @@ public class AdminUsersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_users_list);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.AdminUsersConstraintLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
