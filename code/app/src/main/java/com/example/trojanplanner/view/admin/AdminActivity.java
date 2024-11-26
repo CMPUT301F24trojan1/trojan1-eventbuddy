de3fd188f5e7d533341e5dac90bc6a4f54a77aa1
Madelaine Dalangin <madelainedalangin@gmail.com>
2024-11-25 23:04:05 -0700

    Added ConstraintLayout of RecyclerView for events under admin

--- code/app/src/main/java/com/example/trojanplanner/view/admin/AdminActivity.java
+++ code/app/src/main/java/com/example/trojanplanner/view/admin/AdminActivity.java
@@ -16,8 +16,8 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_admin_events_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.AdminEventsConstraintLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
