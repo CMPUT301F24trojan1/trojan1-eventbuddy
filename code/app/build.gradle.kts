d5a5f16b384eb499e61bb1e04f0a9aa352aea1ba
Arib Amin <arib_amin@yahoo.com>
2024-12-01 23:30:20 -0700

    model unit tests

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -56,6 +56,9 @@ dependencies {
     implementation(libs.activity)
 
     // Test dependencies
+    testImplementation ("org.robolectric:robolectric:4.9")
+    testImplementation("org.mockito:mockito-core:4.11.0")
+    androidTestImplementation("org.mockito:mockito-android:4.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
